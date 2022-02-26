8069673ffbaa3f3f10a2e43189b5fa977a84fb89
sky-coding-12 <akashlathiya121212@gmail.com>
2022-02-26 20:57:40 +0530

    Add TabController Features

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'Screens/myCalls.dart';
+import 'Screens/myCamera.dart';
 import 'Screens/myChats.dart';
 import 'Screens/myStatus.dart';
 
@@ -43,220 +44,132 @@ class _MyHomeState extends State<MyHome> {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).primaryColor,
-        title: const Text(
-          "Whatsapp",
-          style: TextStyle(
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-        actions: [
-          IconButton(
-            tooltip: "search",
-            onPressed: () {},
-            icon: const Icon(
-              Icons.search,
+    double cameraWidth = width / 24;
+    double yourWidth = (width - cameraWidth) / 5;
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).primaryColor,
+          title: const Text(
+            "Whatsapp",
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
             ),
           ),
-          PopupMenuButton(
-            tooltip: "More options",
-            itemBuilder: (context) {
-              return [
-                const PopupMenuItem(
-                  child: Text("New group"),
+          actions: [
+            IconButton(
+              tooltip: "search",
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+              ),
+            ),
+            PopupMenuButton(
+              tooltip: "More options",
+              itemBuilder: (context) {
+                return [
+                  const PopupMenuItem(
+                    child: Text("New group"),
+                  ),
+                  const PopupMenuItem(
+                    child: Text("New broadcast"),
+                  ),
+                  const PopupMenuItem(
+                    child: Text("Linked devices"),
+                  ),
+                  const PopupMenuItem(
+                    child: Text("Starred messages"),
+                  ),
+                  const PopupMenuItem(
+                    child: Text("Payments"),
+                  ),
+                  const PopupMenuItem(
+                    child: Text("Settigns"),
+                  ),
+                ];
+              },
+            )
+          ],
+          bottom: TabBar(
+            indicatorColor: Colors.white,
+            labelPadding: EdgeInsets.symmetric(
+                horizontal: (width - (cameraWidth + yourWidth * 3)) / 8),
+            isScrollable: true,
+            tabs: [
+              SizedBox(
+                width: cameraWidth,
+                child: const Tab(
+                  icon: Icon(Icons.camera_alt),
                 ),
-                const PopupMenuItem(
-                  child: Text("New broadcast"),
-                ),
-                const PopupMenuItem(
-                  child: Text("Linked devices"),
-                ),
-                const PopupMenuItem(
-                  child: Text("Starred messages"),
-                ),
-                const PopupMenuItem(
-                  child: Text("Payments"),
-                ),
-                const PopupMenuItem(
-                  child: Text("Settigns"),
-                ),
-              ];
-            },
-          )
-        ],
-        bottom: PreferredSize(
-          preferredSize: Size(width, 65),
-          child: Container(
-              alignment: Alignment.bottomCenter,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: IconButton(
-                      tooltip: "Camera",
-                      icon: const Icon(
-                        Icons.camera_alt,
-                        size: 25.0,
-                        color: Colors.white60,
+              ),
+              SizedBox(
+                width: yourWidth,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Tab(
+                      text: "CHATS",
+                    ),
+                    const SizedBox(width: 5.0),
+                    Container(
+                      alignment: Alignment.center,
+                      height: 17.0,
+                      width: 17.0,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
                       ),
-                      onPressed: () {},
+                      child: Text(
+                        "8",
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontSize: 12.0,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: yourWidth,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Tab(
+                      text: "STATUS",
                     ),
-                  ),
-                  Expanded(
-                    flex: 6,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              onStatus = false;
-                              onCalls = false;
-                              onChats = true;
-                            });
-                          },
-                          child: Container(
-                            height: 50.0,
-                            alignment: Alignment.center,
-                            width: width / 4.0,
-                            decoration: BoxDecoration(
-                              border: Border(
-                                bottom: BorderSide(
-                                  color: onChats
-                                      ? Colors.white
-                                      : Colors.transparent,
-                                  width: 2.5,
-                                ),
-                              ),
-                            ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  "CHATS",
-                                  style: TextStyle(
-                                    color:
-                                        onChats ? Colors.white : Colors.white60,
-                                    fontSize: 16.0,
-                                  ),
-                                ),
-                                const SizedBox(width: 5.0),
-                                Container(
-                                  alignment: Alignment.center,
-                                  height: 20.0,
-                                  width: 20.0,
-                                  decoration: BoxDecoration(
-                                    color:
-                                        onChats ? Colors.white : Colors.white60,
-                                    borderRadius: BorderRadius.circular(50.0),
-                                  ),
-                                  child: Text(
-                                    "8",
-                                    style: TextStyle(
-                                      fontSize: 10.0,
-                                      color: Theme.of(context).primaryColor,
-                                    ),
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              onStatus = true;
-                              onCalls = false;
-                              onChats = false;
-                            });
-                          },
-                          child: Container(
-                            height: 50.0,
-                            alignment: Alignment.center,
-                            width: width / 4.0,
-                            decoration: BoxDecoration(
-                              border: Border(
-                                bottom: BorderSide(
-                                  color: onStatus
-                                      ? Colors.white
-                                      : Colors.transparent,
-                                  width: 2.5,
-                                ),
-                              ),
-                            ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  "STATUS",
-                                  style: TextStyle(
-                                    color: onStatus
-                                        ? Colors.white
-                                        : Colors.white60,
-                                    fontSize: 16.0,
-                                  ),
-                                ),
-                                const SizedBox(width: 5.0),
-                                Container(
-                                  alignment: Alignment.center,
-                                  height: 6.0,
-                                  width: 6.0,
-                                  decoration: BoxDecoration(
-                                    color: onStatus
-                                        ? Colors.white
-                                        : Colors.white60,
-                                    borderRadius: BorderRadius.circular(50.0),
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              onStatus = false;
-                              onCalls = true;
-                              onChats = false;
-                            });
-                          },
-                          child: Container(
-                            height: 50.0,
-                            alignment: Alignment.center,
-                            width: width / 4.0,
-                            decoration: BoxDecoration(
-                              border: Border(
-                                bottom: BorderSide(
-                                  color: onCalls
-                                      ? Colors.white
-                                      : Colors.transparent,
-                                  width: 2.5,
-                                ),
-                              ),
-                            ),
-                            child: Text(
-                              "CALLS",
-                              style: TextStyle(
-                                color: onCalls ? Colors.white : Colors.white60,
-                                fontSize: 16.0,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              )),
+                    const SizedBox(width: 5.0),
+                    Container(
+                      height: 7.0,
+                      width: 7.0,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: yourWidth,
+                child: const Tab(
+                  text: "CALLS",
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-      body: SafeArea(
-        child: onChats
-            ? const MyChatsScreen()
-            : (onStatus ? const MyStatusScreen() : const MyCallScreen()),
+        body: const TabBarView(
+          children: [
+            MyCameraScreen(),
+            MyChatsScreen(),
+            MyStatusScreen(),
+            MyCallScreen(),
+          ],
+        ),
       ),
     );
   }
